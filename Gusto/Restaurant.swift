ec2f9500cec9759d33d0ea3510fe6d5290bd2d5a
Champ <champ@odds.team>
2024-01-18 10:01:18 +0800

    Create Restaurant Model

---
+++ Gusto/Restaurant.swift
@@ -0,0 +1,15 @@
+//
+//  Restaurant.swift
+//  Gusto
+//
+//  Created by ถิรวุฒิ ไชยชะอุ่ม on 18/1/2567 BE.
+//
+
+import Foundation
+
+class Restaurant {
+    var name: String
+    var priceRating: Int
+    var qualityRating: Int
+    var speedRating: Int
+}
