2d32a99d680d8bf3dbaf99c7f0be1e0cf34ab755
Champ <champ@odds.team>
2024-01-18 10:21:28 +0800

    Add modelcontext and create data

--- Gusto/ContentView.swift
+++ Gusto/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let WokThisWay = Restaurant(name: "Wok this Way", priceRating: 1, qualityRating: 2, speedRating: 3)
+    let ThymeSquare = Restaurant(name: "Thyme Square", priceRating: 2, qualityRating: 3, speedRating: 4)
+    let PastaLaVista = Restaurant(name: "Pasta la Vista", priceRating: 2, qualityRating: 2, speedRating: 3)
+    let LifeOfPie = Restaurant(name: "Life of Pie", priceRating: 4, qualityRating: 4, speedRating: 4)
+    let LoadOfTheWings = Restaurant(name: "Load of the Wings", priceRating: 5, qualityRating: 5, speedRating: 5)
+    @Environment(\.modelContext) var modelContext
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        modelContext.insert(WokThisWay)
+        modelContext.insert(ThymeSquare)
+        modelContext.insert(PastaLaVista)
+        modelContext.insert(LifeOfPie)
+        modelContext.insert(LoadOfTheWings)
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }.toolbar {
+                
+            }
         }
         .padding()
     }
