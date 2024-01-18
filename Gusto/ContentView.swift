8a928bf9ab5de0a2ae034873c90636f0cad70864
Champ <champ@odds.team>
2024-01-18 09:58:54 +0800

    Init Gusto iOS Conf Workshop

---
+++ Gusto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gusto
+//
+//  Created by ถิรวุฒิ ไชยชะอุ่ม on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by ถิรวุฒิ ไชยชะอุ่ม on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
